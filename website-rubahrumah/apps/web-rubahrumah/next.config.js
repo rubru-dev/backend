37a1dbecb5f672a89f29cc40f3a20c7fdfdadc19
Jerry Irawan <cs.rubahrumah@gmail.com>
2026-05-17 21:17:28 +0700

    test

--- website-rubahrumah/apps/web-rubahrumah/next.config.js
+++ website-rubahrumah/apps/web-rubahrumah/next.config.js
@@ -25,6 +25,8 @@ const nextConfig = {
       { protocol: "http", hostname: "localhost", port: "8000" },
       { protocol: "http", hostname: "localhost", port: "4000" },
       { protocol: "https", hostname: "api.rubahrumah.id" },
+      { protocol: "https", hostname: "rubru.id" },
+      { protocol: "https", hostname: "main.rubru.id" },
       { protocol: "https", hostname: "img.youtube.com" },
       ...envRemotePatterns,
     ],
